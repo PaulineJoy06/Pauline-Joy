cffabab1040f621ed203f2b2d9a3063ba0b3d51e
Pauline Joy R. Mingo <116432769+PaulineJoy06@users.noreply.github.com>
2022-11-13 13:26:10 +0800

    Add files via upload

---
+++ Contact tracing using dictionary to store information.py
@@ -0,0 +1,20 @@
+#display menu
+print()
+print("==========M E N U==========")
+print()
+print("1 => Add an item")
+print("2 => Search")
+print("3 => Exit (y/n)")
+print()
+print("===========================")
+
+#ask user input
+user_input = int(input("What do you want to do?"))
+
+#if input is 1, add an item
+if user_input == 1:
+    print()
+#if input is 2, search
+#if input is 3, exit
+
+
