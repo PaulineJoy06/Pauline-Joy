d705a9d11c331076c151683fdec1785797502207
PaulineJoy06 <paulinejoymingo@gmail.com>
2022-11-13 22:07:23 +0800

    Added Personal Informations stored in dictionary

--- Contact tracing using dictionary to store information.py
+++ Contact tracing using dictionary to store information.py
@@ -1,6 +1,12 @@
 print("**********PROGRAMMED BY:**********")
 print("*******Pauline Joy R. Mingo*******")
 
+Personal Informations = []
+Contact 1 = {"Full Name": "Dorothy Mingo", "Age": "45", "Address": "Romblon", "Favorite Music Genre": "Classical"}
+Contact 2 = {"Full Name": "Edgar Justin Laguesma", "Age": "21", "Address": "Romblon", "Favorite Music Genre": "OPM"}
+Contact 3 = {"Full Name": "Katrice Mingo", "Age": "14", "Address": "Makati", "Favorite Music Genre": "Pop"}
+
+
 #display menu
 print()
 print("==========M E N U==========")
@@ -10,6 +16,10 @@ print("2 => Search")
 print("3 => Exit (y/n)")
 print()
 print("===========================")
+
+#Allow users to select item in the menu (check if valid)
+#Perform the selected options
+#Use dictionary to store the information, using full names as key and value is another dictionary of infos
 
 
 
