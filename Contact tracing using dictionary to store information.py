b920933d745b0f3e5ea05ac0d8a0b9686856cb7a
PaulineJoy06 <paulinejoymingo@gmail.com>
2022-11-16 00:04:51 +0800

    changed the dictionary key

--- Contact tracing using dictionary to store information.py
+++ Contact tracing using dictionary to store information.py
@@ -20,7 +20,7 @@ while True:
     userInputFunct = int(input("What do you want to do: 1, 2 or 3? "))
 
 #dictionary
-    contact = {}
+    personaldatadict = {}
 
 #diplay Option  1 (add an item)
     if userInputFunct == 1:
@@ -41,34 +41,35 @@ while True:
         print("Yay! Your informations have been saved!")
         print()
 
+
 #display Option 2 (search for an item)
     elif userInputFunct == 2:
 
-        #assigning keys and their corresponding values in the dictionary
-        contact["Full Name"] = name
-        contact["Age"] = age
-        contact["Address"] = address
-        contact["Contact Number"] = contact_num
-        contact["Vaccination Status"] = vaccination_status
+    #assigning keys and their corresponding values in the dictionary
+        personaldatadict["Full Name"] = name
+        personaldatadict["Age"] = age
+        personaldatadict["Address"] = address
+        personaldatadict["Contact Number"] = contact_num
+        personaldatadict["Vaccination Status"] = vaccination_status
        
 
-        #user input for the viewing of record
+    #user input for the viewing of record
         full_name = input("Enter your full name: ")
         print()
         print("Search result for: ", full_name)
         print()
         
-        #nested if-else statement to compare if the entered data is exisiting 
+    #nested if-else statement to compare if the entered information is exisiting 
         if full_name in name:
-            for key in contact:
-                print(key, ":", contact[key])
+            for key in personaldatadict:
+                print(key, ":", personaldatadict[key])
                 print()
         else: 
             print("The data is not existing. Please try again.")
             print()
 #display Option 3 (exit the program) 
     elif userInputFunct == 3: 
-        exitVerification = input("Are you sure you want to exit the program? ")
+        exitVerification = input("Are you sure you want to exit the program? (Yes or No) ")
         if exitVerification == "Yes":
             print ("THANK YOU FOR USING THIS PROGRAM!")
             break
